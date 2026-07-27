85e7a208dc2d866fcd9a8106930ee20017f96260
Bhawana Yadav <bhawnayadavuk@gmail.com>
2026-07-27 15:57:24 +0100

    Connected frontend to deployed backend

--- frontend/src/lib/apollo.ts
+++ frontend/src/lib/apollo.ts
@@ -1,10 +1,15 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/",
+  uri: "Yhttps://full-stack-ecommerce-shop-with-stripe.onrender.com",
 });
 
-export const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+export const apolloClient =
+  new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
